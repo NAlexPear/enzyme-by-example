f71c817bc34a5dd3123405987ecd27d3d77c7d70
Alex Pearson <alex@alexpear.com>
2018-09-24 17:39:30 -0500

    :sparkles: Add test mocking with Sinon

--- spec/app.spec.jsx
+++ spec/app.spec.jsx
@@ -2,14 +2,24 @@ import { createSandbox } from 'sinon';
 import { expect } from 'chai';
 import { mount, shallow } from 'enzyme';
 import React from 'react';
-import App from '../src/app';
+import App from '../src/app.jsx';
 
 const box = createSandbox();
+
 describe('<App />', () => {
   let app;
+  let mountedApp;
+  let componentDidMount;
+  let callOnMount;
+  let instance;
 
   beforeEach(() => {
+    callOnMount = box.stub(App.prototype, 'callOnMount');
+    componentDidMount = box.spy(App.prototype, 'componentDidMount');
     app = shallow(<App />);
+    mountedApp = mount(<App />);
+
+    instance = app.instance();
   });
 
   afterEach(() => {
@@ -18,5 +28,24 @@ describe('<App />', () => {
 
   it('exists', () => {
     expect(app).to.exist;
+    expect(mountedApp).to.exist;
+  });
+
+  it('calls componentOnMount when shallowly or deeply rendered', () => {
+    expect(componentDidMount).to.have.been.calledTwice;
+  });
+
+  describe('callOnMount', () => {
+    it('is called with phrase on mount', () => {
+      expect(callOnMount).to.have.been.calledWithExactly('I was called on mount!');
+    });
+
+    it('can be called with new phrase', () => {
+      const PHRASE = 'new thing!';
+
+      instance.callOnMount(PHRASE);
+
+      expect(callOnMount).to.have.been.calledWithExactly(PHRASE);
+    });
   });
 });
